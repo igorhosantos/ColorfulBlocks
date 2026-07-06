ac4c5282dcef3ceab2e20bb8368526d8e4115003
Igor <igor.hos@gmail.com>
2026-07-06 11:13:22 -0300

    Adding Game Rules, and deleting unused sprites

--- Assets/Scripts/model/GameplayDataSettings.cs
+++ Assets/Scripts/model/GameplayDataSettings.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace ColorfulBlocks.Model
@@ -7,9 +9,21 @@ namespace ColorfulBlocks.Model
     {
         [SerializeField] private int movements;
         [SerializeField] private int score;
-        
+        [SerializeField] private int gridLine;
+        [SerializeField] private int gridColumn;
+        [SerializeField] private List<BlockType> blockTypes;
         
         public int Movements => movements;
         public int Score => score;
+        public int GridLine => gridLine;
+        public int GridColumn => gridColumn;
+        public List<BlockType> BlockTypes => blockTypes;
+    }
+
+    [Serializable]
+    public struct BlockType
+    {
+        public string Id;
+        public Sprite Sprite;
     }
 }
